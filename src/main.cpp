1e27ec1b25e496d293414d77419b98d58332ceb9
Qudix <17361645+Qudix@users.noreply.github.com>
2021-02-12 03:42:13 -0600

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,62 @@
-#include <locale.h>
-
-#include "skse64/PluginAPI.h"
-#include "skse64_common/skse_version.h"
-#include "skse64/ScaleformCallbacks.h"
-
 #include "Plugin.h"
 #include "Data.h"
 #include "Serialize.h"
 
-#include <shlobj.h>
-
-IDebugLog	gLog;
-
-PluginHandle	g_pluginHandle = kPluginHandle_Invalid;
-
-// SKSE Interfaces
-SKSESerializationInterface * g_serialization = NULL;
-SKSEPapyrusInterface       * g_papyrus = NULL;
-
-extern "C" {
-
-	bool SKSEPlugin_Query(const SKSEInterface * skse, PluginInfo * info) {
-		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\PapyrusUtilDev.log");
-
-		// populate info structure
-		info->infoVersion = PluginInfo::kInfoVersion;
-		info->name = "papyrusutil plugin";
-		info->version = 2;
-
-		// store plugin handle so we can identify ourselves later
-		g_pluginHandle = skse->GetPluginHandle();
-
-		// Don't load in editor.
-		if(skse->isEditor) {
-			return false;
-		}
-
-		// Check if version is right.
-		//else if (skse->runtimeVersion < RUNTIME_VERSION_1_5_53) {
-		else if (skse->runtimeVersion != RUNTIME_VERSION_1_5_97) {
-			_MESSAGE("unsupported runtime version %08X", skse->runtimeVersion);
-			return false;
-		}
-
-		// Get the serialization interface and query its version
-		g_serialization = (SKSESerializationInterface *)skse->QueryInterface(kInterface_Serialization);
-		if(!g_serialization) {
-			_MESSAGE("couldn't get serialization interface");
-			return false;
-		}
-		if(g_serialization->version < SKSESerializationInterface::kVersion) {
-			_MESSAGE("serialization interface too old (%d expected %d)", g_serialization->version, SKSESerializationInterface::kVersion);
-			return false;
-		}
-
-		// get papyrus vm interface
-		g_papyrus = (SKSEPapyrusInterface *)skse->QueryInterface(kInterface_Papyrus);
-		if(!g_papyrus) {
-			_MESSAGE("couldn't get papyrus interface");
-			return false;
-		}
-
-		return true;
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		return false;
 	}
 
-	bool SKSEPlugin_Load(const SKSEInterface * skse) {
-		//setlocale(LC_ALL, "POSIX");
-		_MESSAGE("Loading Version: %d", (int)PAPYRUSUTIL_VERSION);
+	*path /= "PapyrusUtil.log"sv;
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 
-		Data::InitLists();
-		Plugin::InitPlugin();
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
 
-		// Dev tmp
-		//g_serialization->SetUniqueID(g_pluginHandle, 884792 + 15325);
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%s(%#): [%^%l%$] %v"s);
 
-		g_serialization->SetUniqueID(g_pluginHandle, 884715692 + 227106806);
-		g_serialization->SetSaveCallback(g_pluginHandle, Data::Serialization_Save);
-		g_serialization->SetLoadCallback(g_pluginHandle, Data::Serialization_Load);
-		g_serialization->SetRevertCallback(g_pluginHandle, Data::Serialization_Revert);
-		//g_serialization->SetFormDeleteCallback(g_pluginHandle, Data::FormDelete);
+	a_info->infoVersion = SKSE::PluginInfo::kVersion;
+	a_info->name = "PapyrusUtil";
+	a_info->version = Version::MAJOR;
 
-		g_papyrus->Register(Plugin::RegisterFuncs);
-
-
-		return true;
+	if (a_skse->IsEditor()) {
+		logger::critical("Loaded in editor, marking as incompatible"sv);
+		return false;
 	}
+
+	const auto ver = a_skse->RuntimeVersion();
+	if (ver < SKSE::RUNTIME_1_5_39) {
+		logger::critical("Unsupported runtime version {}"sv, ver.string());
+		return false;
+	}
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	logger::info("Loading Version: {}"sv, Version::MAJOR);
+
+	SKSE::Init(a_skse);
+
+	Data::InitLists();
+	Plugin::InitPlugin();
+
+	const auto serialization = SKSE::GetSerializationInterface();
+	// Dev temp
+	// 	serialization->SetUniqueID(884792 + 15325);
+	serialization->SetUniqueID(884715692 + 227106806);
+	serialization->SetSaveCallback(Data::Serialization_Save);
+	serialization->SetLoadCallback(Data::Serialization_Load);
+	serialization->SetRevertCallback(Data::Serialization_Revert);
+	// serialization->SetFormDeleteCallback(Data::FormDelete);
+
+	const auto papyrus = SKSE::GetPapyrusInterface();
+	papyrus->Register(Plugin::RegisterFuncs);
+
+	return true;
 }
